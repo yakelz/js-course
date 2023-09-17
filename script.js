ddfb87c8fbdd668ac2c7841f69b5e6d04edc01d3
yakelz <73719604+yakelz@users.noreply.github.com>
2023-09-17 16:53:48 +0300

    js-todolist

--- script.js
+++ script.js
@@ -1,33 +1,70 @@
-const myForm = document.forms.namedItem('my-form');
-/** @type { HTMLInputElement } */
+const listInput = document.getElementById('listInput');
+const listBtn = document.getElementById('listBtn');
+const list = document.getElementById('list');
+const myArray = document.getElementById('myArray');
 
-if (myForm === null) {
-    throw new Error ("Can't find main form");
+const notes = [
+	{
+		title: 'встать',
+		completed: true,
+	},
+	{
+		title: 'умыться',
+		completed: false,
+	},
+];
+
+function render() {
+	list.innerHTML = '';
+	if (notes.length === 0) {
+		list.innerHTML = '<p> Нет заметок </p>';
+		return;
+	}
+	for (let i = 0; i < notes.length; i++) {
+		addNote(notes[i], i);
+	}
 }
 
-// myForm? - если вернется Null то в text1Input просто будет undefined
-const text1Input = myForm?.elements?.namedItem('text1');
-/** @type { HTMLInputElement | undefined | null } */
-const num1Input = myForm?.elements?.namedItem('num1');
+render();
 
-/** @type { HTMLOutputElement | undefined | null } */
-const output = document.querySelector('output')
+function getNoteTemplate(note, index) {
+	return `
+	<li>
+		<div class="${note.completed ? 'completed' : ''}">
+			${note.title}
+		</div>
+		<div class="buttons">
+			<button data-index="${index}" data-action="toggle">Complete</button>
+			<button data-index="${index}" data-action="delete">Delete</button> 
+		</div>
+	</li>
+	`;
+}
 
-console.log(output.textContent);
+function addNote(note, index) {
+	list.insertAdjacentHTML('beforeend', getNoteTemplate(note, index));
+}
 
+listBtn.onclick = function () {
+	if (!listInput.value) {
+		return;
+	}
+	const newNote = { title: listInput.value, completed: false };
+	notes.push(newNote);
+	render();
+	listInput.value = '';
+};
 
-let text1 = '';
-let num1 = 0;
-
-myForm.addEventListener('submit',
-    (event) => {
-
-        //отменить действите по умолчанию (отправка формы)
-        event.preventDefault();
-
-        text1 = text1Input.value;
-        num1 = num1Input.valueAsNumber;
-        
-        output.textContent = `text1 = ${text1}, num1 = ${num1}`;
-    },
-);
+list.onclick = function (event) {
+	if (!event.target.dataset) {
+		return;
+	}
+	const index = Number(event.target.dataset.index);
+	const action = event.target.dataset.action;
+	if (action === 'delete') {
+		notes.splice(index, 1);
+	} else if (action === 'toggle') {
+		notes[index].completed = !notes[index].completed;
+	}
+	render();
+};
